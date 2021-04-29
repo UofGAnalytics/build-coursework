dccab4bf8b7f5319c16ca013da69b9abe95e1374
David McArthur <david.mcarthur.2@glasgow.ac.uk>
2021-04-29 12:37:27 +0100

    use CSS variables for theming

--- template/src/modal.ts
+++ template/src/modal.ts
@@ -1,19 +1,19 @@
-const $modal = document.getElementById('modal') as Element;
-const $close = document.getElementById('modal-close') as Element;
-const $bg = document.getElementById('modal-bg') as Element;
-const $content = document.getElementById('modal-content') as Element;
+const modal = document.getElementById('modal') as Element;
+const close = document.getElementById('modal-close') as Element;
+const bg = document.getElementById('modal-bg') as Element;
+const content = document.getElementById('modal-content') as Element;
 
-$close.addEventListener('click', closeModal);
-$bg.addEventListener('click', closeModal);
+close.addEventListener('click', closeModal);
+bg.addEventListener('click', closeModal);
 
 export function closeModal() {
-  $modal.classList.remove('show');
+  modal.classList.remove('show');
   setTimeout(() => {
-    $content.innerHTML = '';
+    content.innerHTML = '';
   }, 500);
 }
 
-export function openModal(content: string) {
-  $content.innerHTML = content;
-  $modal.classList.add('show');
+export function openModal(html: string) {
+  content.innerHTML = html;
+  modal.classList.add('show');
 }
