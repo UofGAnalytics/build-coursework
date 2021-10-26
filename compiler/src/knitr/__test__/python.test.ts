a0cbb85228a3b85d198bd4af2d3a3133f69f9200
Dave Star <daviestar@gmail.com>
2021-10-26 17:09:06 +0100

    fix ci

--- compiler/src/knitr/__test__/python.test.ts
+++ compiler/src/knitr/__test__/python.test.ts
@@ -2,12 +2,13 @@ import { testProcessor } from '../../test-utils/test-processor';
 
 // sometimes this works instead ¯\_(ツ)_/¯
 // Sys.setenv(RETICULATE_PYTHON = "/usr/bin/python3")
+// reticulate::use_python("/usr/bin/python3")
 
 describe('python', () => {
   it('should run embedded python', async () => {
     const { md } = await testProcessor(`
       \`\`\`{r setup, echo=FALSE}
-      reticulate::use_python("/usr/bin/python3")
+      Sys.setenv(RETICULATE_PYTHON = "/usr/bin/python3")
       \`\`\`
 
       \`\`\`{python}
