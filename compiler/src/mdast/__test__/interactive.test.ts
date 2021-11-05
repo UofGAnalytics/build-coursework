afb7ea8f8a34f67369d7f2a981c3bd8b85d53cce
Dave Star <daviestar@gmail.com>
2021-11-05 16:35:54 +0000

    interactive elements as markdown images

---
+++ compiler/src/mdast/__test__/interactive.test.ts
@@ -0,0 +1,11 @@
+import { fixtureTestProcessor } from '../../test-utils/fixture-test-processor';
+
+describe('interactive elements', () => {
+  it('should display embedded .html', async () => {
+    const { html } = await fixtureTestProcessor('interactive-elements', {
+      noEmbedAssets: false,
+    });
+
+    expect(html).toContain('<div id="multivariate-normal-demo">');
+  });
+});
