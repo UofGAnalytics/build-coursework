2329d67728cee2ddc5c0ed440a8c8df0020443aa
Dave Star <daviestar@gmail.com>
2021-11-19 16:56:26 +0000

    don't replace mbox when used as intended in LaTeX

--- compiler/src/pre-parse/convert-block-tex.ts
+++ compiler/src/pre-parse/convert-block-tex.ts
@@ -1,8 +1,23 @@
-const blockList = ['\\newpage', '\\pagebreak', '\\mbox'];
+const blockList = ['\\newpage', '\\pagebreak'];
 
 export function convertNewPageToDirective(contents: string) {
   return contents
     .split('\n')
     .map((a) => (blockList.some((b) => a.includes(b)) ? '::pagebreak' : a))
+    .join('\n');
+}
+
+export function convertEmptyMBoxToDirective(contents: string) {
+  return contents
+    .split('\n')
+    .map((line) => {
+      if (
+        line.includes('\\mbox') &&
+        line.replace('{', '').replace('}', '').trim() === '\\mbox'
+      ) {
+        return '::pagebreak';
+      }
+      return line;
+    })
     .join('\n');
 }
