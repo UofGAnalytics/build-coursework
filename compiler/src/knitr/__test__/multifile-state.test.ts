220c9c76eada06d07ab3a93433ded339c002e9b3
Dave Star <david.mcarthur.2@glasgow.ac.uk>
2022-02-15 11:16:07 +0000

    fix test

--- compiler/src/knitr/__test__/multifile-state.test.ts
+++ compiler/src/knitr/__test__/multifile-state.test.ts
@@ -4,7 +4,11 @@ describe('multifile knitr state', () => {
   it('should allow knitr state to pass between files', async () => {
     const { html } = await fixtureTestProcessor('multifile-knitr-state');
     expect(html).toContain('<code>[1] 3</code>');
-    expect(html).toContain('test/1/university-of-glasgow.png');
-    expect(html).toContain('test/2/Media_811846_smxx.jpeg');
+    expect(html.replace(/[\\/]+/g, ' ')).toContain(
+      'test 1 university-of-glasgow.png'
+    );
+    expect(html.replace(/[\\/]+/g, ' ')).toContain(
+      'test 2 Media_811846_smxx.jpeg'
+    );
   });
 });
