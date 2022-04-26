8efc750c3b28db9e76c002d740b49b974937aa56
Dave Star <david.mcarthur.2@glasgow.ac.uk>
2022-04-26 13:14:45 +0100

    assert no image attributes

--- compiler/src/mdast/__test__/images.test.ts
+++ compiler/src/mdast/__test__/images.test.ts
@@ -5,7 +5,7 @@ import {
 
 // <figure class="img-wrapper" id="#my-alt-text">
 //   <div class="img-bg">
-//     <img src="/var/folders/dy/tzrrqxmx7_701rcnq9dnvfvh0000gp/T" alt="My alt text">
+//     <img src="" alt="My alt text">
 //   </div>
 //   <figcaption>
 //     <a href="#my-alt-text">
@@ -34,5 +34,15 @@ describe('images', () => {
     expect(ignoreWhitespace(html)).toMatch(
       /<figureclass="img-wrapper"id="my-alt-text"><divclass="img-bg"><imgsrc=".+"alt="Myalttext"><\/div><figcaption><ahref="#my-alt-text"><spanclass="caption-count">Figure1:<\/span>Myalttext<\/a><\/figcaption><\/figure>/
     );
+  });
+
+  it('should render an html figure with custom attributes', async () => {
+    const { hasWarningMessage } = await testProcessor(`
+      ![My alt text](){width=50%}
+    `);
+
+    expect(
+      hasWarningMessage('image attributes are not supported: {width=50%}')
+    ).toBe(true);
   });
 });
