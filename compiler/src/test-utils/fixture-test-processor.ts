dd52342a41f308bf8786480aa24ee61bc0891833
Dave Star <david.mcarthur.2@glasgow.ac.uk>
2022-05-05 11:55:53 +0100

    try ci

--- compiler/src/test-utils/fixture-test-processor.ts
+++ compiler/src/test-utils/fixture-test-processor.ts
@@ -1,4 +1,4 @@
-import { spawn } from 'child_process';
+import { exec } from 'child_process';
 import path from 'path';
 
 import dargs from 'dargs';
@@ -25,27 +25,19 @@ export async function fixtureTestProcessor(
   );
 
   return new Promise<string>((resolve, reject) => {
-    const args = [path.join('./fixtures', fixture), ...flags];
-    const rmarkdown = spawn('rmarkdown', args);
+    const fixturePath = path.join('./fixtures', fixture);
+    const cmd = `rmarkdown ${fixturePath} ${flags.join(' ')}`;
 
-    const result: string[] = [];
-
-    rmarkdown.stdout.on('data', (data) => {
-      const str = data.toString();
-
-      if (str.startsWith('✖')) {
-        return reject(str);
+    exec(cmd, (err, response, stdErr) => {
+      if (stdErr) {
+        console.log(stdErr);
+        reject(stdErr);
       }
-
-      result.push(str);
-    });
-
-    rmarkdown.stdout.on('end', () => {
-      resolve(result.join(''));
-    });
-
-    rmarkdown.stderr.on('data', (data) => {
-      reject(data.toString());
+      if (err) {
+        console.error(err);
+        reject(err);
+      }
+      resolve(response);
     });
   });
 }
