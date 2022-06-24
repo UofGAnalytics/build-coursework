6d3a79a8acf95a90901273b7b9fa8c22491bbda4
Dave Star <david.mcarthur.2@glasgow.ac.uk>
2022-06-24 10:04:35 +0100

    allow compiler to proceed if it can't read Github latest version

--- compiler/src/utils/check-for-latest-version.ts
+++ compiler/src/utils/check-for-latest-version.ts
@@ -1,10 +1,11 @@
 import chalk from 'chalk';
+import figureSet from 'figures';
 import fetch from 'node-fetch';
 
 const repo = 'UofGAnalytics/build-coursework';
 
 type ReleaseInfo = {
-  tag_name: string;
+  tag_name?: string;
 };
 
 export async function checkForLatestVersion() {
@@ -15,8 +16,17 @@ export async function checkForLatestVersion() {
     `https://api.github.com/repos/${repo}/releases/latest`
   );
   const json = (await response.json()) as ReleaseInfo;
-  const latestTag = json.tag_name.replace('v', '');
   const currentVersion = process.env.VERSION;
+
+  if (json.tag_name === undefined) {
+    const message = `Can't read latest version from Github`;
+    console.log(chalk.yellow.bold(`${figureSet.warning}  ${message}`));
+    console.log(chalk.yellow(`Current version: ${currentVersion}`));
+    console.log('');
+    return;
+  }
+
+  const latestTag = json.tag_name.replace('v', '');
 
   if (latestTag !== currentVersion) {
     console.log(chalk.yellow.bold('New version available'));
