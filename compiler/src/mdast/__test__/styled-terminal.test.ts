61e3c069c8e6dc52f58093c337c39d3b617c9474
Dave Star <david.mcarthur.2@glasgow.ac.uk>
2022-08-11 13:41:18 +0100

    fix test

--- compiler/src/mdast/__test__/styled-terminal.test.ts
+++ compiler/src/mdast/__test__/styled-terminal.test.ts
@@ -7,27 +7,14 @@ describe('styled-terminal', () => {
   it('should render a terminal element', async () => {
     const { html } = await testProcessor(`
       \`\`\`{bash}
-      cd ~/Desktop
-      ls
+      echo "hello!"
       \`\`\`
     `);
 
     const expected = unindentString(`
       <div class="terminal">
-        <pre><code>cd ~/Desktop
-      ls</code></pre>
-        <pre><code>3x3+Customs+Universal+6-in-1+Router+Jig+Instructions+(1).pdf
-      backup.pages
-      basement stairs
-      messages.mbox
-      motivating-example.txt
-      pat.txt
-      progress.pages
-      solar panels
-      solomusic.pdf
-      solomusic_rs232.pdf
-      todo.pages
-      todo.rtf</code></pre>
+        <pre><code>echo "hello!"</code></pre>
+        <pre><code>hello!</code></pre>
       </div>
     `);
 
