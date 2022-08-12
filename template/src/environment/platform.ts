43b4607002cca5732842577f29acc502f8feb1a1
Dave Star <david.mcarthur.2@glasgow.ac.uk>
2022-08-12 15:10:38 +0100

    fix frontend js environment toggling bug

--- template/src/environment/platform.ts
+++ template/src/environment/platform.ts
@@ -1,31 +1,23 @@
 import { saveState } from '../util';
 import { state } from './state';
 
-const current = `platform-${state.platform}`;
-
-document.documentElement.classList.add(current);
+document.documentElement.classList.add(`platform-${state.platform}`);
 
 document.querySelectorAll('#platforms input').forEach((elem) => {
   const input = elem as HTMLInputElement;
   input.checked = input.value === state.platform;
-});
-
-document.querySelectorAll('#platforms label').forEach((elem) => {
-  elem.addEventListener('click', setPlatform);
+  elem.addEventListener('change', setPlatform);
 });
 
 function setPlatform(e: Event) {
-  const target = e.currentTarget as Element;
-  const platform = target.getAttribute('data-platform') as string;
+  const target = e.currentTarget as HTMLInputElement;
+  const platform = target.value as string;
   const newClass = `platform-${platform}`;
 
-  console.log({ platform, newClass });
-
-  if (document.documentElement.classList.contains(newClass)) {
-    return;
+  if (!document.documentElement.classList.contains(newClass)) {
+    const oldClass = `platform-${state.platform}`;
+    document.documentElement.classList.replace(oldClass, newClass);
+    state.platform = platform;
+    saveState('environment', state);
   }
-
-  document.documentElement.classList.replace(current, newClass);
-  state.platform = platform;
-  saveState('environment', state);
 }
