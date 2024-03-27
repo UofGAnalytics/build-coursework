939bee98ee7e074129c480d43199f7ad3e3f751d
David McArthur <david.mcarthur.2@glasgow.ac.uk>
2024-03-27 15:13:42 +0000

    remove commented test

--- compiler/src/mdast/__test__/plot-accessibility-switcher.test.ts
+++ compiler/src/mdast/__test__/plot-accessibility-switcher.test.ts
@@ -42,31 +42,4 @@ describe('plot accessibility switcher', () => {
 
     expect(html).toBe(expected);
   });
-
-  // it('should only show cli', async () => {
-  //   const md = `
-  //     ::::program-switcher
-  //     :::command-line
-  //     I am cli
-  //     :::
-
-  //     :::github-desktop
-  //     I am github desktop
-  //     :::
-  //     ::::
-  //   `;
-  //   const { html } = await testProcessor(md, {
-  //     envProgram: 'command-line',
-  //   });
-
-  //   const expected = unindentString(`
-  //     <div class="program-switcher">
-  //       <div data-program="command-line" class="program show">
-  //         <p>I am cli</p>
-  //       </div>
-  //     </div>
-  //   `);
-
-  //   expect(html).toBe(expected);
-  // });
 });
