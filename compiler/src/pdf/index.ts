08b044573d53e027db6f922bdd5ea76a9e6c18fc
David McArthur <david.mcarthur.2@glasgow.ac.uk>
2024-10-17 13:44:59 +0100

    add page numbers to pdf

--- compiler/src/pdf/index.ts
+++ compiler/src/pdf/index.ts
@@ -1,10 +1,10 @@
 import puppeteer from 'puppeteer';
 
-// const footerTemplate = `
-//   <div style="font-size: 14px; padding-top: 20px; text-align: center; width: 100%;">
-//     Page <span class="pageNumber"></span> of <span class="totalPages"></span>
-//   </div>
-// `;
+const footerTemplate = `
+  <div style="font-family: 'Helvetica'; font-size: 14px; padding-top: 20px; text-align: center; width: 100%;">
+    Page <span class="pageNumber"></span> of <span class="totalPages"></span>
+  </div>
+`;
 
 export async function convertToPdf(html: string) {
   const browser = await puppeteer.launch({
@@ -27,8 +27,8 @@ export async function convertToPdf(html: string) {
   const pdf = await page.pdf({
     format: 'a4',
     printBackground: true,
-    // displayHeaderFooter: true,
-    // footerTemplate,
+    displayHeaderFooter: true,
+    footerTemplate,
     margin: {
       top: '20px',
       left: '40px',
